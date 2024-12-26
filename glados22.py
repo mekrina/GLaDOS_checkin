71ee11e875406f383954968dd75b3678a9d1ad1d
freestyle <2023302181261@whu.edu.cn>
2024-12-26 13:28:40 +0800

    修改脚本

--- glados22.py
+++ glados22.py
@@ -1,17 +1,18 @@
 import requests,json,os
-
-# pushplus秘钥
-sckey = os.environ.get("PUSHPLUS_TOKEN", "")
-sendContent = ''
-# glados账号cookie
-cookies= os.environ.get("GLADOS_COOKIE", []).split("&")
-if cookies[0] == "":
-    print('未获取到COOKIE变量') 
-    cookies = []
-    exit(0)
-
-
-def start():    
+# -------------------------------------------------------------------------------------------
+# github workflows
+# -------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+# pushplus密钥 申请地址 http://www.pushplus.plus
+    sckey = os.environ.get("PUSHPLUS_TOKEN", "")
+# 推送内容
+    sendContent = ''
+# glados账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
+    cookies = os.environ.get("GLADOS_COOKIE", []).split("&")
+    if cookies[0] == "":
+        print('未获取到COOKIE变量') 
+        cookies = []
+        exit(0)
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
@@ -29,18 +30,13 @@ def start():
         email = state.json()['data']['email']
         if 'message' in checkin.text:
             mess = checkin.json()['message']
-            print(email+'----'+mess+'----剩余('+time+')天')  # 日志输出
-            global sendContent
+            print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
             sendContent += email+'----'+mess+'----剩余('+time+')天\n'
         else:
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'更新cookie')
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
+            print('cookie已失效')  # 日志输出
      #--------------------------------------------------------------------------------------------------------#   
     if sckey != "":
-        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title=VPN签到成功'+'&content='+sendContent)
+         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
 
 
-def main_handler(event, context):
-  return start()
-
-if __name__ == '__main__':
-    start()
