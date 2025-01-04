3880ac47b7e51246d85fccd923b1249d18934d38
freestyle <2023302181261@whu.edu.cn>
2025-01-04 23:23:55 +0800

    hhhh

--- glados.py
+++ glados.py
@@ -2,7 +2,6 @@ import requests
 import json
 import os
 
-raise Exception("just a try")
 url = "https://glados.rocks/api/user/checkin"
 data = {"token":"glados.one"}
 cookie = os.environ.get("GLADOS_COOKIE", "")
@@ -10,11 +9,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.141 Safari/537.36",
     "Cookie": cookie
 }
-
-print(cookie)
 response = requests.post(url, data=data, headers=headers)
 try:
     print(json.loads(response.text)['message'])
 except:
-    print(response.text)
-    print("cookie失效")
+    raise Exception("cookie失效")
